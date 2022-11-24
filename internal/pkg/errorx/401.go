07f45baee91179558801b26d3ab9255753451f3c
Sean Zheng <blackhorseya@gmail.com>
2022-11-25 03:42:31 +0800

    feat: add auth required middleware

--- internal/pkg/errorx/401.go
+++ internal/pkg/errorx/401.go
@@ -1,1 +1,12 @@
 package errorx
+
+import (
+	"net/http"
+
+	"github.com/blackhorseya/irent/pkg/errors"
+)
+
+var (
+	// ErrAuthHeaderFormat means must provide Authorization header with format `Bearer {token}`
+	ErrAuthHeaderFormat = errors.New(http.StatusUnauthorized, 40100, "Must provide Authorization header with format `Bearer {token}`")
+)
