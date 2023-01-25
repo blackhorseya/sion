fd3aa1ff421090b25a07db045c0ec0b419e48c86
Sean Zheng <blackhorseya@gmail.com>
2023-01-25 08:51:19 +0800

    feat: impl book car api

--- internal/adapter/order/restful/api/v1/bookings/post_booking.go
+++ internal/adapter/order/restful/api/v1/bookings/post_booking.go
@@ -3,7 +3,13 @@ package bookings
 import (
 	"net/http"
 
+	"github.com/blackhorseya/irent/internal/pkg/errorx"
+	"github.com/blackhorseya/irent/pkg/contextx"
+	rm "github.com/blackhorseya/irent/pkg/entity/domain/rental/model"
+	"github.com/blackhorseya/irent/pkg/httpheaders"
+	"github.com/blackhorseya/irent/pkg/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type bookCarRequest struct {
@@ -25,6 +31,40 @@ type bookCarRequest struct {
 // @Failure 500 {object} er.Error
 // @Router /v1/bookings [post]
 func (i *impl) BookCar(c *gin.Context) {
-	// todo: 2023/1/25|sean|impl me
-	c.JSON(http.StatusOK, nil)
+	ctx, ok := c.MustGet(string(contextx.KeyCtx)).(contextx.Contextx)
+	if !ok {
+		_ = c.Error(errorx.ErrContextx)
+		return
+	}
+
+	token, ok := c.MustGet(string(httpheaders.KeyToken)).(string)
+	if !ok {
+		ctx.Error(errorx.ErrMissingToken.Error())
+		_ = c.Error(errorx.ErrMissingToken)
+		return
+	}
+
+	var data bookCarRequest
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		ctx.Error(errorx.ErrInvalidRental.Error(), zap.Error(err))
+		_ = c.Error(errorx.ErrInvalidRental)
+		return
+	}
+
+	from, err := i.auth.GetByAccessToken(ctx, token)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	// todo: 2023/1/25|sean|refactor: you should be using new function to create car instance
+	target := &rm.Car{Id: data.ID, ProjectId: data.ProjectID}
+	ret, err := i.biz.BookRental(ctx, from, target)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, response.OK.WithData(ret))
 }
