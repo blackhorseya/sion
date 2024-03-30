81aa5424d68f97d27588277655686ab9fbdde0d7
Sean Zheng <blackhorseya@gmail.com>
2024-03-31 04:09:27 +0800

    feat: update car data handling in InfluxDB integration
    - Add constant `database` with value `irent_cars`
    - Add logic to write points to InfluxDB with car data
    - Handle error when writing points to InfluxDB
    - Include car data in InfluxDB points
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/jobs/fetchAvailableCars/main.go
+++ adapter/jobs/fetchAvailableCars/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/InfluxCommunity/influxdb3-go/influxdb3"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/blackhorseya/sion/pkg/configx"
 	"github.com/blackhorseya/sion/pkg/contextx"
 	"github.com/blackhorseya/sion/pkg/logging"
 	"go.uber.org/zap"
+)
+
+const (
+	database = "irent_cars"
 )
 
 var injector *Injector
@@ -18,12 +24,29 @@ func Handler() (events.APIGatewayProxyResponse, error) {
 	ctx := contextx.Background()
 	cars, err := injector.assets.FetchAvailableCars(ctx)
 	if err != nil {
+		ctx.Error("failed to fetch available cars", zap.Error(err))
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 		}, err
 	}
 
-	ctx.Info("fetched available cars", zap.Any("cars", cars[:5]))
+	now := time.Now()
+	var points []*influxdb3.Point
+	for _, car := range cars {
+		points = append(points, influxdb3.NewPointWithMeasurement("available_cars").
+			SetTag("car_id", car.ID).
+			SetDoubleField("latitude", car.Location.Latitude).
+			SetDoubleField("longitude", car.Location.Longitude).
+			SetTimestamp(now))
+	}
+
+	err = injector.influxdb.WritePoints(ctx, points, influxdb3.WithDatabase(database))
+	if err != nil {
+		ctx.Error("failed to write points", zap.Error(err))
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
