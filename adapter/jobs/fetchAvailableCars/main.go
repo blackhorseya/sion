2e8d890644e9136e9c699cfaaed5471a88d81506
Sean Zheng <blackhorseya@gmail.com>
2024-03-31 03:34:05 +0800

    feat: implement scheduled event for fetching available cars
    - Add BUILD.bazel file for the `fetchAvailableCars` job
    - Add main.go file for the `fetchAvailableCars` job
    - Update serverless.yml to include `fetchAvailableCars` function with scheduled event
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/jobs/fetchAvailableCars/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call.
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
